b84914c01466094852f8387c9a2348f0440db1f7
shayan <shayan.workspace@gmail.com>
2024-02-25 10:59:27 +0500

    fibonacci

--- src/main.go
+++ src/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
 func main() {
-	println("Hello World!")
+	n := 10
+	result := fibonacci(n)
+	fmt.Printf("The %dth Fibonacci number is: %d\n", n, result)
 }
